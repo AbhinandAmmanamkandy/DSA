9bc7f20ea92b862865e4b352143423ede124ce19
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-08 16:42:10 +0530

    Factorial

---
+++ 17. Factorial/fact.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * Factorial using recursive function
+ */
+
+int fact(int n){
+    if(n == 0)
+        return 1;
+    return n * fact(n-1);
+}
+
+int main(){
+    int f = fact(5);
+    printf("%d", f);
+    return 0;
+}
