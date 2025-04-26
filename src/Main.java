840d8816169cb5069fa22fbd4107aaaf9e809a5c
TorqueReborn <torquereborn@gmail.com>
2025-04-26 22:38:09 +0530

    Updated Algorithm

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
-    static int[] nums1 = {1, 3, 5, 1, 0, 0};
-    static int[] nums2 = {3, 4, 6};
-    int m = 3;
+    static int[] nums1 = {1, 3, 5, 0, 0, 0};
+    static int[] nums2 = {2, 4, 6};
+    static int m = 3;
     int n = 3;
-    int p = m;
+    static int p = m;
     static int i = 0;
     static int j = 0;
 
@@ -17,34 +17,43 @@ public class Main {
     }
     public void merge() {
         if(nums1[p] == 0) {
-            if(nums1[i] < nums2[i]) {
+            if(nums1[i] < nums2[j]) {
                 i++;
             } else if(nums1[i] > nums2[j]) {
+                nums1[p] = nums1[i];
                 swap(i, nums1, j, nums2);
                 j++;
             }
         } else {
             // Get Minimum of three
-            System.out.println(nums1[i] + " " + nums1[p] + " " + nums2[j]);
-            if(nums1[i] < nums1[p] && nums1[i] < nums2[j]) {
-                System.out.println("first left is min");
-            } else if(nums1[p] < nums1[i] && nums1[p] < nums2[j]) {
-                System.out.println("first mid is min");
-            } else if(nums2[j] < nums1[p] && nums2[j] < nums1[i]) {
-                System.out.println("right is min");
-            } else if(nums1[i] == nums1[p]) {
-                System.out.println("first and mid is equal");
+            if(nums1[i] == nums1[p]) {
+                p++;
             } else if(nums1[p] == nums2[j]) {
-                System.out.println("first mid and right is equal");
+                // Maybe p++?
+                j++;
+            }else if(nums1[i] < nums1[p] && nums1[i] < nums2[j]) {
+                i++;
+            } else if(nums1[p] < nums1[i] && nums1[p] < nums2[j]) {
+                swap(i, nums1, p, nums1);
+                i++;
+            } else if(nums2[j] < nums1[p] && nums2[j] < nums1[i]) {
+                swap(i, nums1, j, nums2);
+                j++;
             }
         }
     }
+    public void loop() {
+        while(i < m+n) {
+            merge();
+        }
+        // Copy Remaining value
+        nums1[i-1] = nums2[j];
+    }
     public static void main(String[] args) {
         Main main = new Main();
-        main.merge();
+        main.loop();
         System.out.println(Arrays.toString(nums1));
         System.out.println(Arrays.toString(nums2));
-        System.out.println(i);
-        System.out.println(j);
+        System.out.println("i: " + i + "\nj: " + j + "\np: " + p);
     }
 }
